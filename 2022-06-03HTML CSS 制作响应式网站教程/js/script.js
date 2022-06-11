eeb8fdde4b5b96a1f545169f5d4009696c0d5c06
us <94792817+us816204@users.noreply.github.com>
2022-06-12 01:25:02 +0800

    Add files via upload

---
+++ 2022-06-03HTML CSS 制作响应式网站教程/js/script.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navlist.classList.toggle('open');
+};
