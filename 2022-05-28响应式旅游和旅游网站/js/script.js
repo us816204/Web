2bd39d50764bd04629fad0879a2673fea617fe71
us <94792817+us816204@users.noreply.github.com>
2022-06-12 01:23:00 +0800

    Add files via upload

---
+++ 2022-05-28响应式旅游和旅游网站/js/script.js
@@ -0,0 +1,18 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+	header.classList.toggle ("sticky", window.scrollY > 0);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navbar.classList.toggle('open');
+};
+
+window.onscroll = () => {
+	menu.classList.remove('bx-x');
+	navbar.classList.remove('open');
+};
